7ec053e332738e8b7facd78a5d97f2d6479b2827
sebastian <sebastian@fosterinteractove.com>
2026-06-09 12:23:05 -0400

    Updated config.js

--- js/config.js
+++ js/config.js
@@ -3,12 +3,12 @@
 // ---------------------------------------------------------------------------
 
 // The Web App /exec URL from Deploy ▸ Manage deployments.
-export const API_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
+export const API_URL = 'https://script.google.com/macros/s/AKfycby30JlIxPfDM-zBK9U2GnVj1Rov1E9TbKMaLQilwW5VVCMzlpb2v4E5ZDoRnODJ0ljH8Q/exec';
 
 // Folder (on this same GitHub Pages site) that serves the seed JSON files.
 // Used only by the one-time "Seed sheet" admin action. Example:
 //   https://yourname.github.io/wc/docs
-export const DATA_BASE_URL = 'PASTE_YOUR_GITHUB_PAGES_DOCS_URL_HERE';
+export const DATA_BASE_URL = 'https://sgalindo88.github.io/gavinchi-wp-pool';
 
 export const NUM_PLAYERS = 5;
 export const TEAMS_PER_PLAYER = 9;
